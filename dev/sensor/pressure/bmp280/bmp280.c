d8f56e25fe92fb1ee331c36e10a94633901a1b79
Eric Holland <hollanderic@users.noreply.github.com>
2020-09-25 11:16:42 -0400

    adding sensor to test i2c

---
+++ dev/sensor/pressure/bmp280/bmp280.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Eric Holland
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT
+ */
+
+#include <dev/i2c.h>
+#include <dev/sensor/bmp280.h>
+
+
---
+++ dev/sensor/pressure/bmp280/include/dev/sensor/bmp280.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Eric Holland
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT
+ */
+
+#ifndef BMP280_I2C_ADDR
+//7-bit address (does not include rd/wr bit)
+#define BMP280_I2C_ADDR 0x77
+#endif
